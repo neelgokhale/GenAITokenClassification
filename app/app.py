1ab383e8f5ef2cc310885e4862ee7fbaefa84c8c
neelgokhale <neelg14@gmail.com>
2023-10-24 20:02:11 -0400

    init

---
+++ app/app.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   app.py
+@Time    :   2023/10/24 19:55:01
+@Author  :   Neel Gokhale
+@Contact :   neelg14@gmail.com
+@License :   (C)Copyright 2020-2021, Neel Gokhale
+'''
+
---
+++ app/utils.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   utils.py
+@Time    :   2023/10/24 19:54:57
+@Author  :   Neel Gokhale
+@Contact :   neelg14@gmail.com
+@License :   (C)Copyright 2020-2021, Neel Gokhale
+'''
